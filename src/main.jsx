d7ada687531398cb9d69f72ee3ddf33acb56a113
Omar Haidar <omar.heder.store@gmail.com>
2025-06-26 14:42:32 +0300

    fix: corrected import path casing for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "@material-tailwind/react";
 import { BrowserRouter } from "react-router-dom";
-import store from "../Redux/Store.js";
+import store from "../Redux/store.js";
+
 import { Provider } from "react-redux";
 createRoot(document.getElementById("root")).render(
     <Provider store={store}>
